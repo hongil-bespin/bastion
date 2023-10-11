9c3400e4eece5cf2cb26b55a84e141865817b7d9
hongil-bespin <90609530+hongil-bespin@users.noreply.github.com>
2023-10-11 10:19:34 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key from Terraform Cloud"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret key from Terraform Cloud"
+}
